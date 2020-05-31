beeadef0c527f24c3650e4ae05c255d414e5cc1c
GBHomeWorks <a.a.mikhaylov2015@gmail.com>
2020-05-31 14:38:00 +0300

    hw5 complit code

--- src/hw5/src/Road.java
+++ src/hw5/src/Road.java
@@ -1,14 +1,43 @@
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Road extends Stage {
-    public Road ( int length) {
-        this .length = length;
-        this .description = "Дорога " + length + " метров" ;
+    private int racerCount = 1;
+
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock writeLock = readWriteLock.writeLock();
+    private final Lock readLock = readWriteLock.readLock();
+
+    public Road(int length) {
+        this.length = length;
+        this.description = "Дорога " + length + " метров";
     }
+
     @Override
-    public void go (Car c) {
+    public void go(Car c) {
         try {
             System.out.println(c.getName() + " начал этап: " + description);
-            Thread.sleep(length / c.getSpeed() * 1000 );
-            System.out.println(c.getName() + " закончил этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+
+            if (this.length == 40) {
+                writeLock.lock();
+                readLock.lock();
+//                semaphore.acquire();
+                System.out.println(c.getName() + " закончил этап: " + description);
+                if (racerCount == 1) { // <-----------------------
+                    System.out.println(c.getName() + " WINNER!");
+                }
+                    racerCount++;
+
+                if(racerCount == 5){
+                    System.out.println( "ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!" );
+                }
+                writeLock.unlock();
+                readLock.unlock();
+//                semaphore.release();
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
