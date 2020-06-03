1f3052f024a2d9d8bf3f4602fe2cd2f05cdd5a00
GBHomeWorks <a.a.mikhaylov2015@gmail.com>
2020-06-03 17:57:06 +0300

    method 1 complit, methid 2 without tests

--- src/hw6/src/main/java/MainClass.java
+++ src/hw6/src/main/java/MainClass.java
@@ -1,18 +1,26 @@
 import java.util.ArrayList;
 
 public class MainClass {
-    public static void main(String[] args) {
-//        public void subArray(int[] arr ){
-        int[] arr = {0, 1, 2, 3, 0, 5, 6, 7, 8, 9};
+    public static boolean q;
 
-        int b = 0;
-        try {
-            for (int i = arr.length - 1; arr[i] != 4; i--) {
-                b = arr.length - i;
+        public int[] newArr ( int[] arr){
+            int b = 0;          // --- длина нового массива subArr
+
+            try {
+                for (int i = arr.length - 1; arr[i] != 4; i--) {     // --- вычисление длины массива
+                    b = arr.length - i;
+                }
+            } catch (IndexOutOfBoundsException e) {    // здесь ловим исключение подкласса RunTimeException
+                b = 0;
+                System.out.println("В массиве нет элемента со значением '4'");
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("В массиве нет элемента со значением '4'");;
+
+            int[] subArr = new int[b];       // ----- объявление  нового массива
+
+            for (int i = 0; i < subArr.length; i++) {        // ---- формирование массива
+                subArr[i] = arr[arr.length - b + i];
+            }
+            return subArr;
         }
-        System.out.println(b);
     }
-}
+
--- src/hw6/src/test/java/CalcTest.java
+++ src/hw6/src/test/java/CalcTest.java
@@ -1,35 +1,61 @@
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.Arguments;
-//import org.junit.jupiter.params.provider.MethodSource;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.stream.Stream;
-//
-//public class CalcTest {
-//    private MainClass calculator;
-//
-//    @BeforeEach
-//    public void init() {
-//        calculator = new MainClass();
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CalcTest {
+    private MainClass mainClass;
+
+    @BeforeEach
+    public void init() {
+        mainClass = new MainClass();
+    }
+
+    @Test
+    public void test1() {
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] returnArr = {5, 6, 7, 8, 9};
+        Assertions.assertArrayEquals(returnArr, mainClass.newArr(arr));
+    }
+    @Test
+    public void test2() {
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 4, 8, 9};
+        int[] returnArr = {8, 9};
+        Assertions.assertArrayEquals(returnArr, mainClass.newArr(arr));
+    }
+    // Получаем обработку исключения и вывод сообщения в косоль >>>
+    @Test
+    public void test3() {
+        int[] arr = {0, 1, 2, 3, 0, 5, 6, 7, 8, 9};
+        int[] returnArr = {};
+        Assertions.assertArrayEquals(returnArr, mainClass.newArr(arr));
+    }
+
+//    public void test3() {
+//        int arr[] = {0, 1, 2, 3, 0, 5, 6, 7, 8, 9};
+//        int returnArr[] = {};
+//        Assertions.assertEquals( returnArr , mainClass.newArr(arr));
 //    }
-//
+//    @CsvSource({
+//            "0, 1, 2, 3, 4, 5, 6, 7, 8, 9",
+////            "1, 1, 2",
+////            "2, 3, 5",
+////            "7, 5, 12",
+////            "12, 12, 24"
+//    })
 //    @ParameterizedTest
-//    @MethodSource( "dataForAddOperation" )
-////    @Timeout (value = 500, unit = TimeUnit.MILLISECONDS)
-//    public void testAddOperation(int a, int b, int result) {
-//        Assertions.assertEquals(result, calculator.add(a, b));
+//    public void massTestAdd(String input, String expected) {
+////        Assertions.assertEquals(result, mainClass.newArr(a));
+//        int[] arr = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
+//        String result = Arrays.toString(mainClass.newArr(arr));
+//        Assertions.assertEquals(expected, result);
 //    }
-//    public static Stream<Arguments> dataForAddOperation() {
-//        List<Arguments> out = new ArrayList<>();
-//        for (int i = 0; i < 10000; i++) {
-//            int a = (int)(Math.random() * 1000);
-//            int b = (int)(Math.random() * 1000);
-//            int result = a + b;
-//            out.add(Arguments.arguments(a, b, result));
-//        }
-//        return out.stream();
-//    }
-//}
+}
